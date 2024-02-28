7740d9de147790aa58477bb3728afc23d4359503
Mitsuru Nakada <mitsuru_nakada@mac.com>
2024-02-28 21:32:53 +0900

    bug-fix: 削除忘れ

--- overlay_rootfs/scripts/network_init.sh
+++ overlay_rootfs/scripts/network_init.sh
@@ -37,7 +37,6 @@ elif [ "0x007a" = "$VENDERID" ]; then
       insmod /atom/system/driver/atbm603x_wifi_sdio.ko
     fi
   fi
-  echo "LOG_ERR=OFF LOG_SCAN=OFF" > /sys/module/atbm603x_wifi_sdio/atbmfs/atbm_printk_mask 2> /dev/null
 elif [ "0x5653" = "$VENDERID" ]; then
   insmod /atom/system/driver/ssv6x5x.ko stacfgpath=/atom/system/driver/ssv6x5x-wifi.cfg
 elif [ "0x424c" = "$VENDERID" ]; then

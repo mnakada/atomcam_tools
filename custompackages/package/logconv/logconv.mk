ca6aa72b03dc3174cec70557d97f4e80440b282e
Mitsuru Nakada <mitsuru_nakada@mac.com>
2024-03-01 23:59:06 +0900

    機能追加: debug用ツール - AtomCamのlogを読むための変換ツールを追加   usage : logconv .<file>

---
+++ custompackages/package/logconv/logconv.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# logconv
+#
+################################################################################
+
+LOGCONV_VERSION = v0.1.0
+LOGCONV_SITE = /src/custompackages/package/logconv
+LOGCONV_DEPENDENCIES =
+LOGCONV_CONF_OPTS =
+LOGCONV_SITE_METHOD = local
+
+define LOGCONV_BUILD_CMDS
+        $(MAKE) -C $(@D)/src TARGET_CROSS="$(TARGET_CROSS)" all
+endef
+
+define LOGCONV_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/logconv $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
---
+++ custompackages/package/logconv/src/logconv.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define KEY 0x09
+
+int main(int argc, char **argv) {
+
+  if(argc != 2) {
+    fprintf(stderr, "usage : %s <log file>\n", argv[0]);
+    exit(-1);
+  }
+  FILE *fp = fopen(argv[1], "r");
+  fseek(fp, 32, SEEK_SET);
+
+  unsigned char x = 0x09;
+  int tc = 0;
+  unsigned long time;
+  while(!feof(fp)) {
+    int d = fgetc(fp);
+    if(d == EOF) break;
+    d ^= KEY;
+    if(!d) continue;
+    if(!tc && (d == '$')) {
+      tc = 1;
+      continue;
+    }
+    if((tc == 1) && (d == '@')) {
+      tc = 2;
+      time = 0;
+      continue;
+    }
+    if((tc >= 2) && (tc < 18)) {
+      tc++;
+      time <<= 4;
+      if((d >= '0') && (d <= '9')) {
+        d -= '0';
+      } else if((d >= 'A') && (d <= 'F')) {
+        d -= '7';
+      } else {
+        tc = 0;
+        putchar(d);
+        continue;
+      }
+      time |= d;
+      if(tc == 18) {
+        char buf[32];
+        time_t t = (time_t)time;
+        strftime(buf, 32, "[%Y/%m/%d-%T]", localtime(&t));
+        printf("%s", buf);
+      }
+      continue;
+    }
+    tc = 0;
+    putchar(d);
+  }
+  fclose(fp);
+}

ada81db9e6248d0bf92aacbce0b6b53d10a3a02a
Mitsuru Nakada <mitsuru_nakada@mac.com>
2024-03-17 18:12:37 +0900

    refactoring: - video_callback 不要なコードの削除

--- libcallback/video_callback.c
+++ libcallback/video_callback.c
@@ -138,48 +138,6 @@ static int video_encode_capture(int ch, struct frames_st *frames) {
   return (video_capture[ch].callback)(frames);
 }
 
-/*
-static int video_encode_capture(int ch, struct frames_st *frames) {
-
-  if(!video_capture[ch].initialized) {
-    video_capture[ch].initialized = 1;
-    int err;
-    video_capture[ch].fd = open(video_capture[ch].device, O_WRONLY, 0777);
-    if(video_capture[ch].fd < 0) fprintf(stderr, "Failed to open V4L2 device: %s\n", video_capture[ch].device);
-  }
-  if((video_capture[ch].fd >= 0) && !video_capture[ch].enable && video_capture[ch].stream) {
-    int type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
-    int err = ioctl(video_capture[ch].fd, VIDIOC_STREAMOFF, &type);
-    if(err < 0) fprintf(stderr, "Unable to perform VIDIOC_STREAMOFF %s: %d\n", video_capture[ch].device, err);
-    video_capture[ch].stream = 0;
-  }
-
-  if((video_capture[ch].fd >= 0) && video_capture[ch].enable) {
-    int type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
-    if(!video_capture[ch].stream) {
-      struct v4l2_format vid_format;
-      int err = ioctl(video_capture[ch].fd, VIDIOC_G_FMT, &vid_format);
-      vid_format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
-      vid_format.fmt.pix.width = video_capture[ch].width;
-      vid_format.fmt.pix.height = video_capture[ch].height;
-      vid_format.fmt.pix.pixelformat = video_capture[ch].format;
-      vid_format.fmt.pix.sizeimage = 0;
-      vid_format.fmt.pix.field = V4L2_FIELD_NONE;
-      vid_format.fmt.pix.bytesperline = 0;
-      vid_format.fmt.pix.colorspace = V4L2_PIX_FMT_YUV420;
-      err = ioctl(video_capture[ch].fd, VIDIOC_S_FMT, &vid_format);
-      if(err < 0) fprintf(stderr, "Unable to set V4L2 %s format: %d\n", video_capture[ch].device, err);
-      err = ioctl(video_capture[ch].fd, VIDIOC_STREAMON, &type);
-      if(err < 0) fprintf(stderr, "Unable to perform VIDIOC_STREAMON %s: %d\n", video_capture[ch].device, err);
-      if(err >= 0) video_capture[ch].stream = 1;
-    }
-    int size = write(video_capture[ch].fd, frames->buf, frames->length);
-    if(size != frames->length) fprintf(stderr,"Stream write error %s: %s\n", video_capture[ch].device, size);
-  }
-  return (video_capture[ch].callback)(frames);
-}
-*/
-
 static int video0_encode_capture(struct frames_st *frames) {
   return video_encode_capture(0, frames);
 }
